5da02a314975da3214964bb0b7e3bb3d1cf86072
Zeke Farwell <zeke@gearx.com>
2021-07-24 17:42:52 -0400

    various adjustments to boundaries, roads, and label placement for low zoom

--- style/constants/color.js
+++ style/constants/color.js
@@ -1,8 +1,13 @@
-var colorMotorway = "hsla(354, 71%, 40%, 1)";
-var colorMotorwayCasing = "hsla(354, 71%, 20%, 1)";
-var colorMotorwayTunnel = "hsla(354, 71%, 90%, 1)";
-var colorMotorwayTunnelCasing = "hsla(354, 71%, 75%, 1)";
+var hueMotorway = 354;
+
+var colorMotorway = `hsla(${hueMotorway}, 71%, 40%, 1)`;
+var colorMotorwayCasing = `hsla(${hueMotorway}, 71%, 20%, 1)`;
+var colorMotorwayTunnel = `hsla(${hueMotorway}, 71%, 90%, 1)`;
+var colorMotorwayTunnelCasing = `hsla(${hueMotorway}, 71%, 75%, 1)`;
 var colorBridgeCasing = "black";
+
+var hueBorder = 0;
+var hueBorderCasing = 281;
 
 var colorBorder = "rgba(123, 119, 119, 1)";
 var colorBorderCasing = "rgba(224, 207, 232, 1)";
